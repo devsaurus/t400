0da29c0f555d2fa942c21943f6b5996a20cdc353
devsaurus <devsaurus@users.noreply.github.com>
2006-06-11 22:18:52 +0000

    added t410
    git-svn-id: https://opencores.org/ocsvn/t400/trunk@143 a4f3cba7-60e0-4e55-9e60-fdb87d4a7454

--- rtl/vhdl/system/t400_system_comp_pack-p.vhd
+++ rtl/vhdl/system/t400_system_comp_pack-p.vhd
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 --
--- $Id: t400_system_comp_pack-p.vhd,v 1.5 2006-06-11 13:48:13 arniml Exp $
+-- $Id: t400_system_comp_pack-p.vhd,v 1.6 2006-06-11 22:18:52 arniml Exp $
 --
 -- Copyright (c) 2006, Arnim Laeuger (arniml@opencores.org)
 --
@@ -56,6 +56,41 @@ package t400_system_comp_pack is
       so_en_o   : out std_logic;
       sk_o      : out std_logic;
       sk_en_o   : out std_logic
+    );
+  end component;
+
+  component t410
+    generic (
+      opt_ck_div_g         : integer := t400_opt_ck_div_16_c;
+      opt_l_out_type_7_g   : integer := t400_opt_out_type_std_c;
+      opt_l_out_type_6_g   : integer := t400_opt_out_type_std_c;
+      opt_l_out_type_5_g   : integer := t400_opt_out_type_std_c;
+      opt_l_out_type_4_g   : integer := t400_opt_out_type_std_c;
+      opt_l_out_type_3_g   : integer := t400_opt_out_type_std_c;
+      opt_l_out_type_2_g   : integer := t400_opt_out_type_std_c;
+      opt_l_out_type_1_g   : integer := t400_opt_out_type_std_c;
+      opt_l_out_type_0_g   : integer := t400_opt_out_type_std_c;
+      opt_d_out_type_3_g   : integer := t400_opt_out_type_std_c;
+      opt_d_out_type_2_g   : integer := t400_opt_out_type_std_c;
+      opt_d_out_type_1_g   : integer := t400_opt_out_type_std_c;
+      opt_d_out_type_0_g   : integer := t400_opt_out_type_std_c;
+      opt_g_out_type_3_g   : integer := t400_opt_out_type_std_c;
+      opt_g_out_type_2_g   : integer := t400_opt_out_type_std_c;
+      opt_g_out_type_1_g   : integer := t400_opt_out_type_std_c;
+      opt_g_out_type_0_g   : integer := t400_opt_out_type_std_c;
+      opt_so_output_type_g : integer := t400_opt_out_type_std_c;
+      opt_sk_output_type_g : integer := t400_opt_out_type_std_c
+    );
+    port (
+      ck_i      : in    std_logic;
+      ck_en_i   : in    std_logic;
+      reset_n_i : in    std_logic;
+      io_l_b    : inout std_logic_vector(7 downto 0);
+      io_d_o    : out   std_logic_vector(3 downto 0);
+      io_g_b    : inout std_logic_vector(3 downto 0);
+      si_i      : in    std_logic;
+      so_o      : out   std_logic;
+      sk_o      : out   std_logic
     );
   end component;
 
@@ -221,6 +256,10 @@ end t400_system_comp_pack;
 -- File History:
 --
 -- $Log: not supported by cvs2svn $
+-- Revision 1.5  2006/06/11 13:48:13  arniml
+-- * t421 added
+-- * fixed generic list for t420 and t420_notri
+--
 -- Revision 1.4  2006/06/05 20:02:46  arniml
 -- use microbus generic
 --
